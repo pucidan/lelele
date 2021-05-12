b3ed7e20129c0edc7bc8c248219f859978ce9419
pucidan <84089985+pucidan@users.noreply.github.com>
2021-05-12 23:41:54 +0800

    Create demo.cpp

---
+++ demo.cpp
@@ -0,0 +1,15 @@
+# message for record
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+# AccessKey ID
+# LTAI5tBG76tijJw9hGLJdijh
+# AccessKey Secret
+# pmxGSj65KV0XHQ7kHhIEmFEimh1Oid
+
+int main()
+{
+  printf("hello world\n");
+}
