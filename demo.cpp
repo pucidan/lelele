0b1fd24dfecdf628c609b5fd632343a032a034d7
pucidan <84089985+pucidan@users.noreply.github.com>
2021-05-12 23:42:36 +0800

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -1,13 +1,15 @@
-# message for record
+// message for record
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
 
-# AccessKey ID
-# LTAI5tBG76tijJw9hGLJdijh
-# AccessKey Secret
-# pmxGSj65KV0XHQ7kHhIEmFEimh1Oid
+/* 
+AccessKey ID
+LTAI5tBG76tijJw9hGLJdijh
+AccessKey Secret
+pmxGSj65KV0XHQ7kHhIEmFEimh1Oid
+*/
 
 int main()
 {
