6110c7bbe5b9f87b27f09cb64831120120ae0dec
pucidan <84089985+pucidan@users.noreply.github.com>
2021-05-17 20:00:45 +0800

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -11,7 +11,14 @@ AccessKey Secret
 pmxGSj65KV0XHQ7kHhIEmFEimh1Oid
 */
 
+/*
+华为云
+AccessKey
+LBFBXOW4YNQT4ICYXWJ4
+icxKrBfRWwsQPcgFUOp0tRJHp9v8xee2BZH61zqp
+
 int main()
 {
   printf("hello world\n");
 }
+
